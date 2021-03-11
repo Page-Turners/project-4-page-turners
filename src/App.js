9f3152435717a9cb5892dc939f97b01bccac4a76
maryam <maryum_shahnawaz@yahoo.com>
2021-03-11 17:34:30 -0500

    worked with google book api and retrieved data from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import firebase from './firebase.js';
+// import firebase from './firebase.js';
+import { useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    axios({
+      url: `https://www.googleapis.com/books/v1/volumes?`,
+      method: 'GET',
+      dataResponse: 'json',
+      params: {
+        key: "AIzaSyDKgwjUjoEPLSTlCBheUgeuCpqTxirPtGM",
+        q: "flowers",
+        inauthor: 'keyes',
+      }
+    }).then((res) => {
+      const booksData = res.data;
+
+    })
+  }, []);
   return <h1>Page Turner App</h1>
-}
+
+};
 
 export default App
