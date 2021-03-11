8df9a1937dbbfd42f96517c531ff367ff90490f7
maryam <maryum_shahnawaz@yahoo.com>
2021-03-11 14:51:46 -0500

    added h1 to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import firebase from './firebase.js';
 import './App.css';
-=======
-import logo from './logo.svg'
-import './App.css'
->>>>>>> 96fa5e36c9375bad7e8134aaa9c97512e527266f
 
 function App() {
   return <h1>Page Turner App</h1>
