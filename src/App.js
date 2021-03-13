54b075dd481cf432265fdf337d11b57eb09baa80
ragulan <ragulanravi21@gmail.com>
2021-03-13 16:45:43 -0500

    added search function, and added code for app.js data filtering

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
 // import firebase from './firebase.js';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'
 import Search from './Search'
 
 function App() {
-  const [book, setBook] = useState('');
+  // books is an array
+  const [book, setBook] = useState([])
+  // results is an empty array
+  const [result, setResult] = useState([])
+
   useEffect(() => {
     axios({
       url: `https://www.googleapis.com/books/v1/volumes?`,
       method: 'GET',
       dataResponse: 'json',
       params: {
-        key: "AIzaSyDKgwjUjoEPLSTlCBheUgeuCpqTxirPtGM",
-        q: "derrida",
+        key: 'AIzaSyDKgwjUjoEPLSTlCBheUgeuCpqTxirPtGM',
+        q: 'derrida',
         inauthor: 'keyes',
-      }
+      },
     }).then((res) => {
-      const booksData = res.data;
-      const booksDataArray= booksData.items
+      //
+      const booksData = res.data
+      console.log(res.data)
+      const booksDataArray = booksData.items
       console.log(booksData.items)
-      booksDataArray.map((bookObj)=> {
+      booksDataArray.map((bookObj) => {
         // console.log(bookObj.volumeInfo.title, bookObj.volumeInfo.authors)
       })
-
+      console.log(book)
     })
-  }, [book]);
-  return(
+  }, [])
+
+  return (
     <>
-    <h1>Page Turner App</h1>
-    <Search searchBook = {book} /> 
+      <h1>Page Turner App</h1>
+      <Search searchBook={book} />
     </>
-    
-  ) 
-
-
-};
+  )
+}
 
 export default App
--- src/Search.js
+++ src/Search.js
@@ -1,33 +1,51 @@
+import { useEffect, useState } from 'react'
+
 function Search(props) {
-    const searchBook = props
-    
-    const handleSubmit = (event) => {
-        const selectedRadio = document.querySelector('input[type ="radio"]:checked').id
-        event.preventDefault();
-        console.log(selectedRadio);
-        
-    }
+  const searchBook = props
+  const [userChoice, setUserChoice] = useState('placeholder')
+  //   handles submit
+  const handleSubmit = (event) => {
+    const selectedRadio = document.querySelector('input[type ="radio"]:checked')
+      .id
+    event.preventDefault()
+    console.log(selectedRadio)
+    // stores the search query
+    const searchQuery = event.target[0].value
+    setUserChoice(searchQuery)
+  }
 
-    const handleRadioOption = (event) => {
-        console.log(event)
-    }
+  // const handleRadioOption = (event) => {
+  //   console.log(event)
+  // }
 
-    return(
-       <div>
-           <form action="" onSubmit={handleSubmit}>
-           <label htmlFor="bookSearch">What would you like to read?</label>
-           <input type="text" id="bookSearch"/>
+  return (
+    <div>
+      <form action='' onSubmit={handleSubmit}>
+        <label htmlFor='bookSearch'>What would you like to read?</label>
+        <input type='text' id='bookSearch' value={userChoice} />
 
-           <label htmlFor="author">Author</label>
-                <input type="radio" id="author" value="author" name="searchType" onChange={handleRadioOption}/>
+        <label htmlFor='author'>Author</label>
+        <input
+          type='radio'
+          id='author'
+          value='author'
+          name='searchType'
+          // onChange={handleRadioOption}
+        />
 
-           <label htmlFor="title">Title</label>
-           <input type="radio" id="title" value="title" name="searchType" onChange={handleRadioOption}/>
+        <label htmlFor='title'>Title</label>
+        <input
+          type='radio'
+          id='title'
+          value='title'
+          name='searchType'
+          // onChange={handleRadioOption}
+        />
 
-           <button>Find Me A Book! </button>
-            </form>
-       </div>
-    )
+        <button>Find Me A Book! </button>
+      </form>
+    </div>
+  )
 }
 
-export default Search;
+export default Search
